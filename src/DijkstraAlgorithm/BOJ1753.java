5c720ceb7d36e0ab72422b9cbf8fc99fc2647e6c
kyh5037 <srar2398@naver.com>
2022-02-03 12:14:57 +0900

    220203 add BOJ_1753(Dijkstra)

--- src/DijkstraAlgorithm/BOJ1753.java
+++ src/DijkstraAlgorithm/BOJ1753.java
@@ -1,2 +1,69 @@
-package DijkstraAlgorithm;public class BOJ1753 {
+package DijkstraAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Node implements Comparable<Node> {
+    int vertex;
+    int weight;
+    Node(int vertex, int weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return weight - o.weight;
+    }
+}
+
+public class BOJ1753 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
+
+        ArrayList<Node> list[] = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list[u].add(new Node(v, w));
+        }
+
+        int[] dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            for (Node nextNode : list[curNode.vertex]) {
+                if (dist[nextNode.vertex] > dist[curNode.vertex] + nextNode.weight) {
+                    dist[nextNode.vertex] = dist[curNode.vertex] + nextNode.weight;
+                    pq.offer(new Node(nextNode.vertex, dist[nextNode.vertex]));
+                }
+            }
+        }
+
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] != Integer.MAX_VALUE) {
+                System.out.println(dist[i]);
+            } else {
+                System.out.println("INF");
+            }
+        }
+    }
+
 }
